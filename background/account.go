2aa00683980df71eb3dd926f679fe133f284238a
Anh Nguyen <anhnguyen@bitmark.com>
2020-02-12 11:50:40 +0700

    Delete fb archive when deleting accounts

--- background/account.go
+++ background/account.go
@@ -7,6 +7,8 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/bitmark-inc/spring-app-api/store"
+	"github.com/getsentry/sentry-go"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
@@ -14,51 +16,73 @@ import (
 func (b *BackgroundContext) deleteUserData(ctx context.Context, accountNumber string) error {
 	logEntity := log.WithField("prefix", "delete_user_data")
 
+	// Delete on postgres db
+	logEntity.Info("Remove postgres db")
+	if err := b.store.DeleteFBArchives(ctx, &store.FBArchiveQueryParam{
+		AccountNumber: &accountNumber,
+	}); err != nil {
+		logEntity.Error(err)
+		sentry.CaptureException(err)
+	}
+	if err := b.store.DeleteAccount(ctx, accountNumber); err != nil {
+		logEntity.Error(err)
+		sentry.CaptureException(err)
+	}
+
 	// Delete parser's data_owner
 	logEntity.Info("Remove data owner on bitsocial")
 	if err := b.bitSocialClient.DeleteDataOwner(ctx, accountNumber); err != nil {
-		return err
+		logEntity.Error(err)
+		sentry.CaptureException(err)
 	}
 
 	// Delete data on dynamodb
 	logEntity.Info("Remove post week stat")
 	if err := b.fbDataStore.RemoveFBStat(ctx, accountNumber+"/post-week-stat"); err != nil {
-		return err
+		logEntity.Error(err)
+		sentry.CaptureException(err)
 	}
 
 	logEntity.Info("Remove post year stat")
 	if err := b.fbDataStore.RemoveFBStat(ctx, accountNumber+"/post-year-stat"); err != nil {
-		return err
+		logEntity.Error(err)
+		sentry.CaptureException(err)
 	}
 
 	logEntity.Info("Remove post decade stat")
 	if err := b.fbDataStore.RemoveFBStat(ctx, accountNumber+"/post-decade-stat"); err != nil {
-		return err
+		logEntity.Error(err)
+		sentry.CaptureException(err)
 	}
 
 	logEntity.Info("Remove reaction week stat")
 	if err := b.fbDataStore.RemoveFBStat(ctx, accountNumber+"/reaction-week-stat"); err != nil {
-		return err
+		logEntity.Error(err)
+		sentry.CaptureException(err)
 	}
 
 	logEntity.Info("Remove reaction year stat")
 	if err := b.fbDataStore.RemoveFBStat(ctx, accountNumber+"/reaction-year-stat"); err != nil {
-		return err
+		logEntity.Error(err)
+		sentry.CaptureException(err)
 	}
 
 	logEntity.Info("Remove reaction decade stat")
 	if err := b.fbDataStore.RemoveFBStat(ctx, accountNumber+"/reaction-decade-stat"); err != nil {
-		return err
+		logEntity.Error(err)
+		sentry.CaptureException(err)
 	}
 
 	logEntity.Info("Remove posts")
 	if err := b.fbDataStore.RemoveFBStat(ctx, accountNumber+"/post"); err != nil {
-		return err
+		logEntity.Error(err)
+		sentry.CaptureException(err)
 	}
 
 	logEntity.Info("Remove reactions")
 	if err := b.fbDataStore.RemoveFBStat(ctx, accountNumber+"/reaction"); err != nil {
-		return err
+		logEntity.Error(err)
+		sentry.CaptureException(err)
 	}
 
 	// Delete on s3
@@ -72,14 +96,10 @@ func (b *BackgroundContext) deleteUserData(ctx context.Context, accountNumber st
 	})
 
 	if err := s3manager.NewBatchDeleteWithClient(svc).Delete(ctx, iter); err != nil {
-		return err
+		logEntity.Error(err)
+		sentry.CaptureException(err)
 	}
 
-	// Delete on postgres db
-	logEntity.Info("Remove postgres db")
-	if err := b.store.DeleteAccount(ctx, accountNumber); err != nil {
-		return err
-	}
 	logEntity.Info("Finish")
 
 	return nil
